8a630f537bef8047991eef19fe51486b9663b8c4
James McFadden <jamesmcfadden111@gmail.com>
2021-09-17 16:06:00 -0400

    Binary and Martian

--- src/main/java/com/techelevator/DecimalToBinary.java
+++ src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,30 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a series of decimal numbers separated by single spaces: ");
 
+		String line = scanner.nextLine();
+		String[] decimalNumbers = line.split(" ");
+
+		for (String num : decimalNumbers) {
+
+			int originalDecimal = Integer.parseInt(num);
+			String binary = "";
+			int decimal = originalDecimal;
+
+			while (decimal > 0) {
+				int tempOneOrZero = decimal % 2;
+				String oneOrZero = String.valueOf(tempOneOrZero);
+				binary = oneOrZero + binary;
+				decimal /= 2;
+			}
+			System.out.println(originalDecimal + " in binary is: " + binary);
+		}
 	}
 
 }
--- src/main/java/com/techelevator/MartianWeight.java
+++ src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -8,7 +10,7 @@ package com.techelevator;
 Write a command line program which accepts a series of Earth weights from the user  
 and displays each Earth weight as itself, and its Martian equivalent.
 
- $ MartianWeight 
+ $ MartianWeight
  
 Enter a series of Earth weights (space-separated): 98 235 185
  
@@ -16,9 +18,22 @@ Enter a series of Earth weights (space-separated): 98 235 185
  235 lbs. on Earth, is 88 lbs. on Mars.
  185 lbs. on Earth, is 69 lbs. on Mars. 
  */
+
 public class MartianWeight {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter a series of weights on earth, separated by single spaces, in lbs: ");
+		String line = scanner.nextLine();
+		String[] earthWeights = line.split(" ");
+
+		for (String earthWeightString : earthWeights) {
+			int earthWeight = Integer.parseInt(earthWeightString);
+			int marsWeight = (int)(earthWeight * 0.378);
+			System.out.println(earthWeight + " lbs. on Earth equals " + marsWeight + " lbs. on Mars.");
+		}
 
 	}
 
